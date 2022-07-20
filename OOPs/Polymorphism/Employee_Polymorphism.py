a1698b0c1160973d1c04e4fa8eb6d999674a12a8
Snehal Thakur <snehal.hadoop@gmail.com>
2022-07-20 08:37:33 +0530

    Add files via upload

---
+++ OOPs/Polymorphism/Employee_Polymorphism.py
@@ -0,0 +1,14 @@
+class IBM:
+    def employeeInfo(self):
+        print("Employees belongs to IBM")
+
+
+class Wipro:
+    def employeeInfo(self):
+        print("Employees belongs tp Wipro")
+
+ibm = IBM()
+ibm.employeeInfo()
+
+wipro = Wipro()
+wipro.employeeInfo()
